3d01e0488b41804db24470861caede211a93098e
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-04-18 01:29:19 +0800

    Update sessionManager.js
    Adding setTimeout so that the save scroll position function runs after the hero animation is done.

--- sessionManager.js
+++ sessionManager.js
@@ -1,23 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-  console.log("Script loaded")
-  
-  const savedPosition = sessionStorage.getItem('scrollPosition');
-  if (savedPosition) {
-    console.log('Scrolling to saved position:', savedPosition);
-    window.scrollTo(0, parseInt(savedPosition, 10));
-  }
+    var enterButton = document.querySelector('.enter-button'); // Adjust the selector as needed
+    if (enterButton) {
+        enterButton.addEventListener('click', function() {
+            setTimeout(() => {
+                let savedPosition = parseInt(sessionStorage.getItem('scrollPosition'), 10);
+                if (!isNaN(savedPosition) && savedPosition > 0) {
+                    window.scrollTo(0, savedPosition);
+                    console.log('Scrolling to saved position:', savedPosition);
+                }
+            }, 5550); // Delay set for the hero animation duration
+        });
+    }
 
-  window.addEventListener('beforeunload', function() {
-    const scrollY = window.scrollY || window.pageYOffset;
-    console.log('Saving scroll position:', scrollY);
-    sessionStorage.setItem('scrollPosition', scrollY.toString());
-  });
+    window.addEventListener('beforeunload', function() {
+        sessionStorage.setItem('scrollPosition', window.scrollY || window.pageYOffset);
+    });
 });
-
-window.onload = function() {
-  const savedPosition = sessionStorage.getItem('scrollPosition');
-  if (savedPosition) {
-    window.scrollTo(0, parseInt(savedPosition, 10));
-  }
-};
-
