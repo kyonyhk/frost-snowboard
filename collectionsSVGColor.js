e91ca01c8101eec59d30de8044be203bb1f8cd8d
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-04-19 03:24:26 +0800

    Update collectionsSVGColor.js
    Updating the code to target the paths within .cp-main_svg.

--- collectionsSVGColor.js
+++ collectionsSVGColor.js
@@ -1,18 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
   var svgElement = document.querySelector('.cp-main_svg-wrap svg');
-
   console.log("SVG Element:", svgElement); // Check if the SVG element is being selected
 
   if (svgElement) {
     var currentUrl = window.location.href;
     console.log("Current URL:", currentUrl); // Log the current URL
 
+    var paths = svgElement.querySelectorAll('path'); // Select all path elements within the SVG
     if (currentUrl.includes('ember-collection')) {
-      svgElement.style.fill = '#D97848';
-      console.log("Color set to #D97848");
-    } else if (currentUrl.includes('nebula-collection')) {
-      svgElement.style.fill = '#580DEB';
-      console.log("Color set to #580DEB");
+      paths.forEach(path => path.style.fill = '#D97848');
+      console.log("Color set to #D97848 for all paths");
+    } else (currentUrl.includes('nebula-collection')) {
+      paths.forEach(path => path.style.fill = '#580DEB');
+      console.log("Color set to #580DEB for all paths");
+    } else if (currentUrl.includes('apex-collection')) {
+      paths.forEach(path => path.style.fill = '#6BE688');
+      console.log("Color set to #6BE688 for all paths");
     }
   } else {
     console.log("SVG element not found");
