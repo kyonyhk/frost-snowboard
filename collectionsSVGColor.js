f26fdadca9ca8efdecd11b450a47c6b68248847a
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-04-19 03:10:47 +0800

    Update collectionsSVGColor.js
    Update code to add console.log to debug issue.

--- collectionsSVGColor.js
+++ collectionsSVGColor.js
@@ -1,15 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
   var svgElement = document.querySelector('.cp-main_svg');
 
+  console.log("SVG Element:", svgElement); // Check if the SVG element is being selected
+
   if (svgElement) {
     var currentUrl = window.location.href;
+    console.log("Current URL:", currentUrl); // Log the current URL
 
     if (currentUrl.includes('ember-collection')) {
-      // Change color for Ember Collection
       svgElement.style.fill = '#D97848';
+      console.log("Color set to #D97848");
     } else if (currentUrl.includes('nebula-collection')) {
-      // Change color for Nebula Collection
       svgElement.style.fill = '#580DEB';
+      console.log("Color set to #580DEB");
     }
+  } else {
+    console.log("SVG element not found");
   }
 });
