27cdcb9369b357c46fdfbb5abce8b878a5ac424e
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-07-14 05:01:01 +0800

    Update webglWaveEffect.js
    Removing import statements.

--- webglWaveEffect.js
+++ webglWaveEffect.js
@@ -1,7 +1,3 @@
-import * as THREE from 'three';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-
 gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener('DOMContentLoaded', () => {
