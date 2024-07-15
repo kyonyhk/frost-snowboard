7291d3157d7a635d4c80fa51becb75a3724f6003
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-07-15 11:50:57 +0800

    Update collectionsBackButtonColor.js
    Updating the code to handle hover state and svg color changes.

--- collectionsBackButtonColor.js
+++ collectionsBackButtonColor.js
@@ -1,22 +1,43 @@
 document.addEventListener("DOMContentLoaded", function() {
   var currentUrl = window.location.href;
+  var backWrap = document.querySelector('.collections-back_wrap');
   var targetTexts = document.querySelectorAll('.collections-back_wrap .h-h6.is-cta.is-collections-back');
   var targetSVG = document.querySelector('.collections-back_wrap .cta-svg');
 
   // Function to apply styles
   function applyStyles(color, opacity) {
-    targetSVG.style.fill = color; // Change the fill color of the SVG
+    if (targetSVG) {
+      targetSVG.style.fill = color; // Change the fill color of the SVG
+    }
     targetTexts.forEach(function(element) {
       element.style.color = `rgba(${parseInt(color.slice(1, 3), 16)}, ${parseInt(color.slice(3, 5), 16)}, ${parseInt(color.slice(5, 7), 16)}, ${opacity})`; // Change text fill color with opacity
       element.style.webkitTextStroke = `1px ${color}`; // Stroke color with full opacity
     });
   }
 
+  // Function to handle hover effect
+  function handleHover(color) {
+    if (backWrap) {
+      backWrap.addEventListener('mouseenter', function() {
+        applyStyles(color, 1); // Full opacity on hover
+      });
+      backWrap.addEventListener('mouseleave', function() {
+        applyStyles(color, 0.2); // 20% opacity on mouse leave
+      });
+    }
+  }
+
+  let color;
   if (currentUrl.includes('apex-collection')) {
-    applyStyles('#A1FCCF', 0.2);
+    color = '#A1FCCF';
   } else if (currentUrl.includes('ember-collection')) {
-    applyStyles('#FDFDCE', 0.2);
+    color = '#FDFDCE';
   } else if (currentUrl.includes('nebula-collection')) {
-    applyStyles('#877FCB', 0.2);
+    color = '#877FCB';
+  }
+
+  if (color) {
+    applyStyles(color, 0.2); // Initial state with 20% opacity
+    handleHover(color); // Set up hover effect
   }
 });
