fb8a05e681ae936b11e83e219c170a6f61c386d1
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-07-17 12:12:16 +0800

    Update collectionsImageEffects.js
    Adding back the shader code.

--- collectionsImageEffects.js
+++ collectionsImageEffects.js
@@ -17,42 +17,40 @@ document.addEventListener('DOMContentLoaded', function() {
         },
         fragment: `
             void main() {
-                gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); // Render everything red
+                uniform float time;
+                uniform float progress;
+                uniform float intensity;
+                uniform sampler2D texture1;
+                uniform sampler2D texture2;
+                uniform sampler2D displacement;
+                uniform vec4 resolution;
+                varying vec2 vUv;
+    
+                mat2 getRotM(float angle) {
+                    float s = sin(angle);
+                    float c = cos(angle);
+                    return mat2(c, -s, s, c);
+                }
+    
+                const float PI = 3.1415;
+                const float angle1 = PI * 0.25;
+                const float angle2 = -PI * 0.75;
+    
+                void main() {
+                    vec2 newUV = (vUv - vec2(0.5)) * resolution.zw + vec2(0.5);
+                    vec4 disp = texture2D(displacement, newUV);
+                    vec2 dispVec = vec2(disp.r, disp.g);
+    
+                    vec2 distortedPosition1 = newUV + getRotM(angle1) * dispVec * intensity * progress;
+                    vec4 t1 = texture2D(texture1, distortedPosition1);
+    
+                    vec2 distortedPosition2 = newUV + getRotM(angle2) * dispVec * intensity * (1.0 - progress);
+                    vec4 t2 = texture2D(texture2, distortedPosition2);
+    
+                    gl_FragColor = mix(t1, t2, progress);
             }
         `
     });
-
-    // uniform float time;
-    //         uniform float progress;
-    //         uniform float intensity;
-    //         uniform sampler2D texture1;
-    //         uniform sampler2D texture2;
-    //         uniform sampler2D displacement;
-    //         uniform vec4 resolution;
-    //         varying vec2 vUv;
-
-    //         mat2 getRotM(float angle) {
-    //             float s = sin(angle);
-    //             float c = cos(angle);
-    //             return mat2(c, -s, s, c);
-    //         }
-
-    //         const float PI = 3.1415;
-    //         const float angle1 = PI * 0.25;
-    //         const float angle2 = -PI * 0.75;
-
-    //         void main() {
-    //             vec2 newUV = (vUv - vec2(0.5)) * resolution.zw + vec2(0.5);
-    //             vec4 disp = texture2D(displacement, newUV);
-    //             vec2 dispVec = vec2(disp.r, disp.g);
-
-    //             vec2 distortedPosition1 = newUV + getRotM(angle1) * dispVec * intensity * progress;
-    //             vec4 t1 = texture2D(texture1, distortedPosition1);
-
-    //             vec2 distortedPosition2 = newUV + getRotM(angle2) * dispVec * intensity * (1.0 - progress);
-    //             vec4 t2 = texture2D(texture2, distortedPosition2);
-
-    //             gl_FragColor = mix(t1, t2, progress);
 
     function setCanvasSize() {
         const rect = mainImageContainer.getBoundingClientRect();
