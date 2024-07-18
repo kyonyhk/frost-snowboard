4ec8977724c600bf49739e543ab7693c010ac4dc
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-07-19 02:52:59 +0800

    Update frostTechIntroAnimation.js
    Setting elements to be in initial positions for intro animation.

--- frostTechIntroAnimation.js
+++ frostTechIntroAnimation.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   const techParagraphs = document.querySelectorAll('.paragraph.is-terminal.is-tech');
-  const terminalIcons = document.querySelectorAll('.terminal-icon');
-  const techBorderDivs = document.querySelectorAll('.tech_border-div');
+  const terminalIcon = document.querySelectorAll('.terminal-icon');
+  const techBorderDiv = document.querySelectorAll('.tech_border-div');
   const techHeaders = document.querySelectorAll('.h-h5.is-tech');
   const quakeshiftHeading = document.querySelector('.h-h6.is-tech.is-active.is-quakeshift');
   const quakeshiftNumber = document.querySelector('.s-s4.is-tech.is-quakeshift'); // Assuming this is the correct class
@@ -15,10 +15,28 @@ document.addEventListener('DOMContentLoaded', function() {
   const counterFirstDigit = document.querySelector('.tech_counter-digit-wrap.is-first-digit');
   const counterSecondDigit = document.querySelector('.tech_counter-digit-wrap.is-second-digit');
 
+  // Initial positions for intro animation
+  gsap.set(termainalIcon, {
+    opacity: 0,
+    rotate: '0deg',
+    x: '-300%',
+  });
+  gasp.set(techParagraphs, { y: '100%' });
+  gsap.set(techBorderDiv, { scale: 1.1, opacity: 0 });
+  gasp.set(techHeader, { y: '100%' });
+  gsap.set(quakeshiftHeading, { y: '100%' });
+  gsap.set(quakeshiftNumber, { x: '-100%' });
+  gsap.set(quakeshiftParagraph, { y: '100%' }); 
+  gsap.set(thermofluxHeading, { y: '100%' });
+  gsap.set(thermofluxNumber, { x: '-100%' });
+  gsap.set(flexiweaveHeading, { y: '100%' });
+  gsap.set(counterFirstDigit, { y: '100%' });
+  gsap.set(counterSecondDigit, { y: '100%' });
+  
   const tl = gsap.timeline({ defaults: { ease: "power4.out" } });
 
   // Animate terminal icons
-  tl.from(terminalIcons, {
+  tl.from(terminalIcon, {
     opacity: 0,
     rotate: '0deg',
     x: '-300%', // Moving from -300% on the x-axis
@@ -30,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function() {
     stagger: 0.2, // Stagger of 0.2 seconds between the start of each element's animation
     duration: 0.5,
   }, "-=0.5") // Start immediately after terminalIcons with no delay
-  .from(techBorderDivs, {
+  .from(techBorderDiv, {
     opacity: 0,
     scale: 1.1,
     duration: 0.5
