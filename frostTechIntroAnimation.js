82396cadb4ca70175f7e412a597849058bbc1018
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-07-19 03:05:11 +0800

    Update frostTechIntroAnimation.js
    Using gsap.to instead of gsap.from since I am using gsap.set to set the initial position of the elements.

--- frostTechIntroAnimation.js
+++ frostTechIntroAnimation.js
@@ -37,58 +37,57 @@ document.addEventListener('DOMContentLoaded', function() {
   const tl = gsap.timeline({ defaults: { ease: "power4.out" } });
 
   // Animate terminal icons
-  tl.from(terminalIcon, {
-    opacity: 0,
-    rotate: '0deg',
-    x: '-300%', // Moving from -300% on the x-axis
+  tl.to(terminalIcon, {
+    opacity: 1,
+    rotate: '45deg',
+    x: '0%', // Moving from -300% on the x-axis
     duration: 0.5
   })
-  .from(techParagraphs, {
-    y: '100%', // Starts the animation from 100% below their original position
-    autoAlpha: 0,
+  .to(techParagraphs, {
+    y: '0%', // Starts the animation from 100% below their original position
     stagger: 0.2, // Stagger of 0.2 seconds between the start of each element's animation
     duration: 0.5,
   }, "-=0.5") // Start immediately after terminalIcons with no delay
-  .from(techBorderDiv, {
-    opacity: 0,
-    scale: 1.1,
+  .to(techBorderDiv, {
+    opacity: 1,
+    scale: 1.0,
     duration: 0.5
   }, "-=0.5") // Overlap with the end of techParagraphs animation
   .from(techHeaders, {
-    y: '100%',
+    y: '0%',
     stagger: 0.2,
     duration: 0.5
   }, "-=0.5") // Starts simultaneously with techBorderDivs
   .from(quakeshiftHeading, {
-    y: '100%',
+    y: '0%',
     duration: 0.5
   })
   .from(quakeshiftNumber, {
-    x: '-100%',
+    x: '0%',
     duration: 0.5
   }, "<")
   .from(quakeshiftParagraph, {
-    y: '100%',
+    y: '0%',
     duration: 0.5
   }, "<")
   .from(thermofluxHeading, {
-    y: '100%',
+    y: '0%',
     duration: 0.5
   }, ">")
   .from(thermofluxNumber, {
-    x: '-100%',
+    x: '0%',
     duration: 0.5
   }, "<")
   .from(flexiweaveHeading, {
-    y: '100%',
+    y: '0%',
     duration: 0.5
   }, ">")
   .from(flexiweaveNumber, {
-    x: '-100%',
+    x: '0%',
     duration: 0.5
   }, "<")
   .from([counterFirstDigit, counterSecondDigit], {
-    y: '100%',
+    y: '0%',
     stagger: 0.2,
     duration: 0.5
   });
