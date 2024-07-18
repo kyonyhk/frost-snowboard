c20c2bfbf8c135c2595a122d37df7dce057fe5ed
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-07-19 02:33:15 +0800

    Update frostTechIntroAnimation.js
    Updating timings for techParagraphs.

--- frostTechIntroAnimation.js
+++ frostTechIntroAnimation.js
@@ -4,13 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
   const techBorderDivs = document.querySelectorAll('.tech_border-div');
   const techHeaders = document.querySelectorAll('.h-h5.is-tech');
   const quakeshiftHeading = document.querySelector('.h-h6.is-tech.is-active.is-quakeshift');
-  const quakeshiftNumber = document.querySelector('.s-s4.is-tech.is-quakeshift'); // Corrected the selector to use class for h4 equivalent
+  const quakeshiftNumber = document.querySelector('.s-s4.is-tech.is-quakeshift'); // Assuming this is the correct class
   const quakeshiftParagraph = document.querySelector('.paragraph.p-p2.is-tech.is-quakeshift');
   const thermofluxHeading = document.querySelector('.h-h6.is-tech.is-active.is-thermoflux');
-  const thermofluxNumber = document.querySelector('.s-s4.is-tech.is-thermoflux'); // Corrected the selector to use class for h4 equivalent
+  const thermofluxNumber = document.querySelector('.s-s4.is-tech.is-thermoflux'); // Assuming this is the correct class
   const thermofluxParagraph = document.querySelector('.paragraph.p-p2.is-tech.is-thermoflux');
   const flexiweaveHeading = document.querySelector('.h-h6.is-tech.is-active.is-flexiweave');
-  const flexiweaveNumber = document.querySelector('.s-s4.is-tech.is-flexiweave'); // Corrected the selector to use class for h4 equivalent
+  const flexiweaveNumber = document.querySelector('.s-s4.is-tech.is-flexiweave'); // Assuming this is the correct class
   const flexiweaveParagraph = document.querySelector('.paragraph.p-p2.is-tech.is-flexiweave');
   const counterFirstDigit = document.querySelector('.tech_counter-digit-wrap.is-first-digit');
   const counterSecondDigit = document.querySelector('.tech_counter-digit-wrap.is-second-digit');
@@ -26,19 +26,20 @@ document.addEventListener('DOMContentLoaded', function() {
   })
   .from(techParagraphs, {
     y: '100%', // Starts the animation from 100% below their original position
-    stagger: 0.2, // Delay of 0.2 seconds between the start of each element's animation
+    autoAlpha: 0,
+    stagger: 0.2, // Stagger of 0.2 seconds between the start of each element's animation
     duration: 0.5,
   }, "+=0.1") // Adding a slight delay before starting the paragraphs animation
   .from(techBorderDivs, {
     opacity: 0,
     scale: 1.1,
     duration: 0.5
-  }, "+=0.5") // Starts right after the techParagraphs animations complete
+  }, "-=0.5") // Overlap with the end of techParagraphs animation
   .from(techHeaders, {
     y: '100%',
     stagger: 0.2,
     duration: 0.5
-  }, "+=0.2") // Sequential delay between the headers animations
+  }, "-=0.5") // Starts simultaneously with techBorderDivs
   .from(quakeshiftHeading, {
     y: '100%',
     duration: 0.5
@@ -46,11 +47,11 @@ document.addEventListener('DOMContentLoaded', function() {
   .from(quakeshiftNumber, {
     x: '-100%',
     duration: 0.5
-  }, "<") // Start animation at the same time as the previous
+  }, "<")
   .from(quakeshiftParagraph, {
     y: '100%',
     duration: 0.5
-  }, "<") // Start animation at the same time as the previous
+  }, "<")
   .from(thermofluxHeading, {
     y: '100%',
     duration: 0.5
