662b68c5746bb25d54dd41cdae9cebf07fe3c817
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-07-18 14:46:09 +0800

    Update collectionsImageEffects.js
    Updating file to remove import and to use image from .cp_main-image-container as data-images attribute.

--- collectionsImageEffects.js
+++ collectionsImageEffects.js
@@ -1,5 +1,3 @@
-import gsap from 'gsap';
-
 class Sketch {
   constructor(opts) {
     this.scene = new THREE.Scene();
@@ -245,50 +243,60 @@ class Sketch {
 
 export { Sketch };
 
-let sketch = new Sketch({
-  debug: true,
-  uniforms: {
-    intensity: { value: 0.1, type: 'f', min: 0, max: 3 },
-  },
-  fragment: `
-    uniform float time;
-  uniform float progress;
-  uniform float intensity;
-  uniform float width;
-  uniform float scaleX;
-  uniform float scaleY;
-  uniform float transition;
-  uniform float radius;
-  uniform float swipe;
-  uniform sampler2D texture1;
-  uniform sampler2D texture2;
-  uniform sampler2D displacement;
-  uniform vec4 resolution;
-  varying vec2 vUv;
-  mat2 getRotM(float angle) {
-      float s = sin(angle);
-      float c = cos(angle);
-      return mat2(c, -s, s, c);
+document.addEventListener('DOMContentLoaded', function() {
+  const canvasContainer = document.getElementById('main-image-canvas');
+  const mainImage = document.querySelector('.cp_main-image-container img');
+  
+  if (mainImage && canvasContainer) {
+      // Set the data-images attribute dynamically based on the img src
+      const imageSrc = mainImage.src;
+      canvasContainer.setAttribute('data-images', JSON.stringify([imageSrc]));
   }
-  const float PI = 3.1415;
-  const float angle1 = PI *0.25;
-  const float angle2 = -PI *0.75;
-
-
-  void main()	{
-    vec2 newUV = (vUv - vec2(0.5))*resolution.zw + vec2(0.5);
-
-    vec4 disp = texture2D(displacement, newUV);
-    vec2 dispVec = vec2(disp.r, disp.g);
-
-    vec2 distortedPosition1 = newUV + getRotM(angle1) * dispVec * intensity * progress;
-    vec4 t1 = texture2D(texture1, distortedPosition1);
-
-    vec2 distortedPosition2 = newUV + getRotM(angle2) * dispVec * intensity * (1.0 - progress);
-    vec4 t2 = texture2D(texture2, distortedPosition2);
-
-    gl_FragColor = mix(t1, t2, progress);
-
-  }
-  `,
-});
+  let sketch = new Sketch({
+    debug: true,
+    uniforms: {
+      intensity: { value: 0.1, type: 'f', min: 0, max: 3 },
+    },
+    fragment: `
+      uniform float time;
+      uniform float progress;
+      uniform float intensity;
+      uniform float width;
+      uniform float scaleX;
+      uniform float scaleY;
+      uniform float transition;
+      uniform float radius;
+      uniform float swipe;
+      uniform sampler2D texture1;
+      uniform sampler2D texture2;
+      uniform sampler2D displacement;
+      uniform vec4 resolution;
+      varying vec2 vUv;
+      mat2 getRotM(float angle) {
+          float s = sin(angle);
+          float c = cos(angle);
+          return mat2(c, -s, s, c);
+      }
+      const float PI = 3.1415;
+      const float angle1 = PI *0.25;
+      const float angle2 = -PI *0.75;
+    
+    
+      void main()	{
+        vec2 newUV = (vUv - vec2(0.5))*resolution.zw + vec2(0.5);
+    
+        vec4 disp = texture2D(displacement, newUV);
+        vec2 dispVec = vec2(disp.r, disp.g);
+    
+        vec2 distortedPosition1 = newUV + getRotM(angle1) * dispVec * intensity * progress;
+        vec4 t1 = texture2D(texture1, distortedPosition1);
+    
+        vec2 distortedPosition2 = newUV + getRotM(angle2) * dispVec * intensity * (1.0 - progress);
+        vec4 t2 = texture2D(texture2, distortedPosition2);
+    
+        gl_FragColor = mix(t1, t2, progress);
+    
+      }
+    `,
+  });
+})
