bcff58ac91e8aa41a6cb7ba5209cd1225beabbcd
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-07-21 16:35:17 +0800

    Create frostTechDescriptionHoverBarba.js
    Initial commit.

---
+++ frostTechDescriptionHoverBarba.js
@@ -0,0 +1,27 @@
+function initializeFrostTechDescriptionHover() {
+  document.addEventListener('DOMContentLoaded', () => {
+    const techHeaders = document.querySelectorAll('.tech_description-header-wrap');
+  
+    techHeaders.forEach(header => {
+      header.addEventListener('mouseenter', () => {
+        if (!header.closest('.tech_description-container').classList.contains('is-active')) {
+          gsap.to(header, {
+            opacity: 0.5,
+            duration: 0.5,
+            ease: "power4.inOut"
+          });
+        }
+      });
+  
+      header.addEventListener('mouseleave', () => {
+        if (!header.closest('.tech_description-container').classList.contains('is-active')) {
+          gsap.to(header, {
+            opacity: 0.2,
+            duration: 0.3,
+            ease: "power4.inOut"
+          });
+        }
+      });
+    });
+  });
+}
