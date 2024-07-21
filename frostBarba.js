e51a00a428b2f285d7adf7ce39ae6589b72c9579
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-07-21 13:27:36 +0800

    Create frostBarba.js
    Initial commit for barba transitions.

---
+++ frostBarba.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function () {
+  barba.init({
+    transitions: [
+      {
+        name: 'page-transition',
+        enter(data) {
+          // Ensure the class is added before the animation starts
+          $(data.current.container).addClass('fixed');
+          $(data.next.container).addClass('fixed');
+
+          // Animate the current container opacity
+          gsap.to(data.current.container, {
+            opacity: 0,
+            duration: 1,
+            ease: 'power4.out',
+          });
+
+          // Return the promise of the animation of the next container
+          return gsap.from(data.next.container, {
+            opacity: 0,
+            duration: 1,
+            ease: 'power4.out',
+            onComplete: () => {
+              $(data.next.container).removeClass('fixed');
+            },
+          });
+        },
+      },
+    ],
+    views: [
+      {
+        namespace: 'collections-cms',
+        afterEnter() {
+          console.log('ANIMATION!!!');
+        },
+      },
+      {
+        namespace: 'frost-tech-page',
+        afterEnter() {
+          console.log('ANIMATION!!!');
+        },
+      },
+    ],
+  });
+});
