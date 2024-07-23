8bd829c19a2a0ede266226d9daa2c182e1bbca4e
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-07-24 03:17:01 +0800

    Update navbarHome.js
    Using attr from gsap for stroke fill and adding console log to debug.

--- navbarHome.js
+++ navbarHome.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
     gsap.to(arrowIcon, { opacity: 1.0, duration: 0.5, ease: 'power4.inOut' });
     gsap.to(bigCircle, { opacity: 1.0, duration: 0.5, ease: 'power4.inOut' });
     gsap.to(menuText, { opacity: 1.0, duration: 0.5, ease: 'power4.inOut' });
+    console.log('Applying fill gradient #paint1_linear_3240_2372');
     gsap.to(navbarBackgroundPath, { attr: { fill: 'url(#paint1_linear_3240_2372)' }, duration: 0.5, ease: 'power4.inOut' });
   });
 
@@ -22,6 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
     gsap.to(arrowIcon, { opacity: 0.5, duration: 0.3, ease: 'power4.inOut' });
     gsap.to(bigCircle, { opacity: 0.5, duration: 0.3, ease: 'power4.inOut' });
     gsap.to(menuText, { opacity: 0.5, duration: 0.3, ease: 'power4.inOut' });
+    console.log('Reverting fill gradient to #paint0_linear_3227_209');
     gsap.to(navbarBackgroundPath, { attr: { fill: 'url(#paint0_linear_3227_209)' }, duration: 0.3, ease: 'power4.inOut' });
   });
 
@@ -32,6 +34,7 @@ document.addEventListener('DOMContentLoaded', function() {
     gsap.to(bigCircle, { scale: 1.2, duration: 0.5, ease: 'power4.inOut', fill: '#6BE688', opacity: 1.0 });
     gsap.to(smallCircle, { scale: 0.8, duration: 0.5, ease: 'power4.inOut', opacity: 1.0 });
     gsap.to(arrowIcon.querySelector('path'), { strokeWidth: 2, duration: 0.5, ease: 'power4.inOut' });
+    console.log('Applying fill gradient #paint1_linear_3240_2418');
     gsap.to(navbarBackgroundPath, { attr: { fill: 'url(#paint1_linear_3240_2418)' }, duration: 0.5, ease: 'power4.inOut' });
   });
 
@@ -41,6 +44,7 @@ document.addEventListener('DOMContentLoaded', function() {
     gsap.to(bigCircle, { scale: 1, duration: 0.3, ease: 'power4.inOut', fill: '', opacity: 0.5 });
     gsap.to(smallCircle, { scale: 1, duration: 0.3, ease: 'power4.inOut', opacity: 0.5 });
     gsap.to(arrowIcon.querySelector('path'), { strokeWidth: 1, duration: 0.3, ease: 'power4.inOut' });
+    console.log('Reverting fill gradient to #paint0_linear_3227_209');
     gsap.to(navbarBackgroundPath, { attr: { fill: 'url(#paint0_linear_3227_209)' }, duration: 0.3, ease: 'power4.inOut' });
   });
 });
