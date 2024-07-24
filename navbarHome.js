68dcec88509e8f47c6282f32cb69443bed8c7ac0
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-07-24 13:32:20 +0800

    Update navbarHome.js
    Fixing syntax error.

--- navbarHome.js
+++ navbarHome.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
     navbarBackgroundPath,
     backButton,
     textContainers,
-    svgElement,
+    fillSvgElement,
     defaultPath,
     expandedPath
   });
