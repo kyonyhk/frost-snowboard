c221a564d83329e13d5a8e318665240451409ba1
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-07-28 01:47:38 +0800

    Update navbarHome.js
    Added close menu animation.

--- navbarHome.js
+++ navbarHome.js
@@ -8,16 +8,22 @@ document.addEventListener('DOMContentLoaded', function () {
   const backButton = document.querySelector('.global-navbar_back-button');
   const backLink = document.querySelector('.global-navbar_back-link');
   const linkContainers = document.querySelectorAll('.global-navbar-link');
-  const navbarContainer = document.querySelector('.global-navbar_navbar-container');
+  const navbarContainer = document.querySelector(
+    '.global-navbar_navbar-container'
+  );
   const iconContainer = document.querySelector('.global-navbar-link.is-icon');
   const closeIcon = document.querySelector('.global-navbar_close-icon');
 
-  const fillSvgElement = document.querySelector('.global-navbar_background-fill svg');
+  const fillSvgElement = document.querySelector(
+    '.global-navbar_background-fill svg'
+  );
   const fillGElement = fillSvgElement.querySelector('g');
   const defaultFillPath = document.querySelector('#defaultFillPath');
   const expandedFillPath = document.querySelector('#expandedFillPath');
 
-  const strokeSvgElement = document.querySelector('.global-navbar_background-stroke svg');
+  const strokeSvgElement = document.querySelector(
+    '.global-navbar_background-stroke svg'
+  );
   const strokeGElement = fillSvgElement.querySelector('g');
   const defaultStrokePath = document.querySelector('#defaultStrokePath');
   const expandedStrokePath = document.querySelector('#expandedStrokePath');
@@ -56,7 +62,9 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function updateNavbarDisplay() {
-    const isHomepage = window.location.hostname === 'frost-snow.com' && window.location.pathname === '/';
+    const isHomepage =
+      window.location.hostname === 'frost-snow.com' &&
+      window.location.pathname === '/';
 
     if (isHomepage) {
       if (diamondElement) {
@@ -76,7 +84,7 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   updateNavbarDisplay();
-  
+
   // Check if the back link element exists
   if (backLink) {
     // Add a click event listener
@@ -91,38 +99,85 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
-
   // Text container hover effect
   linkContainers.forEach((container) => {
     container.addEventListener('mouseenter', function () {
       gsap.to(arrowIcon, { opacity: 1.0, duration: 0.5, ease: 'power4.inOut' });
       gsap.to(bigCircle, { opacity: 1.0, duration: 0.5, ease: 'power4.inOut' });
       gsap.to(container, { opacity: 1.0, duration: 0.5, ease: 'power4.inOut' });
-      gsap.to(strokePath, { opacity: 0.3, duration: 0.5, ease: 'power4.inOut' });
-      gsap.to(diamondElement, { opacity: 1.0, duration: 0.5, ease: 'power4.inOut' });
+      gsap.to(strokePath, {
+        opacity: 0.3,
+        duration: 0.5,
+        ease: 'power4.inOut',
+      });
+      gsap.to(diamondElement, {
+        opacity: 1.0,
+        duration: 0.5,
+        ease: 'power4.inOut',
+      });
     });
 
     container.addEventListener('mouseleave', function () {
       gsap.to(arrowIcon, { opacity: 0.5, duration: 0.3, ease: 'power4.inOut' });
       gsap.to(bigCircle, { opacity: 0.5, duration: 0.3, ease: 'power4.inOut' });
       gsap.to(container, { opacity: 0.5, duration: 0.3, ease: 'power4.inOut' });
-      gsap.to(strokePath, { opacity: 0.1, duration: 0.3, ease: 'power4.inOut' });
-      gsap.to(diamondElement, { opacity: 0.5, duration: 0.3, ease: 'power4.inOut' });
+      gsap.to(strokePath, {
+        opacity: 0.1,
+        duration: 0.3,
+        ease: 'power4.inOut',
+      });
+      gsap.to(diamondElement, {
+        opacity: 0.5,
+        duration: 0.3,
+        ease: 'power4.inOut',
+      });
     });
   });
 
   // Back button hover effect
   backButton.addEventListener('mouseenter', function () {
-    gsap.to(bigCircle, { scale: 1.2, opacity: 1.0, duration: 0.5, ease: 'power4.inOut', fill: '#6BE688' });
-    gsap.to(smallCircle, { scale: 0.8, opacity: 1.0, duration: 0.5, ease: 'power4.inOut' });
-    gsap.to(arrowIcon, { strokeWidth: 2, opacity: 1.0, duration: 0.5, ease: 'power4.inOut' });
+    gsap.to(bigCircle, {
+      scale: 1.2,
+      opacity: 1.0,
+      duration: 0.5,
+      ease: 'power4.inOut',
+      fill: '#6BE688',
+    });
+    gsap.to(smallCircle, {
+      scale: 0.8,
+      opacity: 1.0,
+      duration: 0.5,
+      ease: 'power4.inOut',
+    });
+    gsap.to(arrowIcon, {
+      strokeWidth: 2,
+      opacity: 1.0,
+      duration: 0.5,
+      ease: 'power4.inOut',
+    });
     gsap.to(strokePath, { opacity: 0.5, duration: 0.5, ease: 'power4.inOut' });
   });
 
   backButton.addEventListener('mouseleave', function () {
-    gsap.to(bigCircle, { scale: 1, opacity: 0.5, duration: 0.3, ease: 'power4.inOut', fill: '#A1FCCF' });
-    gsap.to(smallCircle, { scale: 1, opacity: 0.5, duration: 0.3, ease: 'power4.inOut' });
-    gsap.to(arrowIcon, { strokeWidth: 1, opacity: 0.5, duration: 0.3, ease: 'power4.inOut' });
+    gsap.to(bigCircle, {
+      scale: 1,
+      opacity: 0.5,
+      duration: 0.3,
+      ease: 'power4.inOut',
+      fill: '#A1FCCF',
+    });
+    gsap.to(smallCircle, {
+      scale: 1,
+      opacity: 0.5,
+      duration: 0.3,
+      ease: 'power4.inOut',
+    });
+    gsap.to(arrowIcon, {
+      strokeWidth: 1,
+      opacity: 0.5,
+      duration: 0.3,
+      ease: 'power4.inOut',
+    });
     gsap.to(strokePath, { opacity: 0.1, duration: 0.3, ease: 'power4.inOut' });
   });
 
@@ -141,7 +196,7 @@ document.addEventListener('DOMContentLoaded', function () {
       boxShadow: 'none',
       rotation: 45,
       duration: 0.3,
-      ease: 'power4.inOut'
+      ease: 'power4.inOut',
     });
   });
 
@@ -195,25 +250,34 @@ document.addEventListener('DOMContentLoaded', function () {
 
   menuContainer.addEventListener('click', function () {
     console.log('Menu text click');
-  
+
     const menuOriginalText = menuContainer.querySelector('.is-original-text');
-    const menuOriginalSplit = new SplitType(menuOriginalText, { types: 'chars' });
+    const menuOriginalSplit = new SplitType(menuOriginalText, {
+      types: 'chars',
+    });
     const menuAnimatedText = menuContainer.querySelector('.is-animated-text');
-    const menuAnimatedSplit = new SplitType(menuAnimatedText, { types: 'chars' });
-  
-    menuOpenTimeline = gsap.timeline()
+    const menuAnimatedSplit = new SplitType(menuAnimatedText, {
+      types: 'chars',
+    });
+
+    menuOpenTimeline = gsap
+      .timeline()
       .to(menuOriginalSplit.chars, {
         y: '-200%',
         stagger: 0.1,
         duration: 0.5,
         ease: 'power4.out',
       })
-      .to(menuAnimatedSplit.chars, {
-        y: '-200%',
-        stagger: 0.1,
-        duration: 0.5,
-        ease: 'power4.out',
-      }, 0)
+      .to(
+        menuAnimatedSplit.chars,
+        {
+          y: '-200%',
+          stagger: 0.1,
+          duration: 0.5,
+          ease: 'power4.out',
+        },
+        0
+      )
       .add(() => {
         // Hide menuContainer and show other containers and icons
         gsap.set(menuContainer, { display: 'none' });
@@ -224,53 +288,89 @@ document.addEventListener('DOMContentLoaded', function () {
           }
         });
       }, '-=0.5') // Start during the exit animation
-  
-      .to(navbarContainer, {
-        width: '577px',
-        duration: 1,
-        ease: 'power4.inOut',
-      }, 0)
-      .to(defaultFillPath, {
-        morphSVG: expandedFillPath,
-        duration: 1,
-        ease: 'power4.inOut',
-      }, 0)
-      .to(fillSvgElement, {
-        attr: { viewBox: '0 0 640 64' },
-        duration: 1,
-        ease: 'power4.inOut',
-      }, 0)
-      .to(fillSvgElement, {
-        width: 640,
-        duration: 1,
-        ease: 'power4.inOut',
-      }, 0)
-      .to(fillGElement, {
-        attr: { filter: 'url(#expandedBackgroundFilter)' },
-        duration: 1,
-        ease: 'power4.inOut',
-      }, 0)
-      .to(defaultStrokePath, {
-        morphSVG: expandedStrokePath,
-        duration: 1,
-        ease: 'power4.inOut',
-      }, 0)
-      .to(strokeSvgElement, {
-        attr: { viewBox: '0 0 640 64' },
-        duration: 1,
-        ease: 'power4.inOut',
-      }, 0)
-      .to(strokeSvgElement, {
-        width: 640,
-        duration: 1,
-        ease: 'power4.inOut',
-      }, 0)
-      .to([iconContainer, linkContainers], {
-        opacity: 1,
-        duration: 0.5,
-        ease: 'power4.inOut',
-      }, 0) // Fading in the containers and icon
-  
+
+      .to(
+        navbarContainer,
+        {
+          width: '577px',
+          duration: 1,
+          ease: 'power4.inOut',
+        },
+        0
+      )
+      .to(
+        defaultFillPath,
+        {
+          morphSVG: expandedFillPath,
+          duration: 1,
+          ease: 'power4.inOut',
+        },
+        0
+      )
+      .to(
+        fillSvgElement,
+        {
+          attr: { viewBox: '0 0 640 64' },
+          duration: 1,
+          ease: 'power4.inOut',
+        },
+        0
+      )
+      .to(
+        fillSvgElement,
+        {
+          width: 640,
+          duration: 1,
+          ease: 'power4.inOut',
+        },
+        0
+      )
+      .to(
+        fillGElement,
+        {
+          attr: { filter: 'url(#expandedBackgroundFilter)' },
+          duration: 1,
+          ease: 'power4.inOut',
+        },
+        0
+      )
+      .to(
+        defaultStrokePath,
+        {
+          morphSVG: expandedStrokePath,
+          duration: 1,
+          ease: 'power4.inOut',
+        },
+        0
+      )
+      .to(
+        strokeSvgElement,
+        {
+          attr: { viewBox: '0 0 640 64' },
+          duration: 1,
+          ease: 'power4.inOut',
+        },
+        0
+      )
+      .to(
+        strokeSvgElement,
+        {
+          width: 640,
+          duration: 1,
+          ease: 'power4.inOut',
+        },
+        0
+      )
+      .to(
+        [iconContainer, linkContainers],
+        {
+          opacity: 1,
+          duration: 0.5,
+          ease: 'power4.inOut',
+        },
+        0
+      ) // Fading in the containers and icon
+
       .add(() => {
         linkContainers.forEach((container) => {
           if (container !== menuContainer) {
@@ -283,12 +383,131 @@ document.addEventListener('DOMContentLoaded', function () {
             });
           }
         });
-  
+
         gsap.fromTo(
           closeIcon,
           { scale: 1.1 },
           { scale: 1.0, duration: 0.5, ease: 'power4.out' }
         );
       }, '-=0.5');
+  });
+
+  // Close navbar animation
+  closeIcon.addEventListener('click', function () {
+    const linkOriginalTexts = document.querySelectorAll(
+      '.global-navbar_text-container .is-original-text'
+    );
+    const linkAnimatedTexts = document.querySelectorAll(
+      '.global-navbar_text-container .is-animated-text'
+    );
+
+    // Animate out the texts and the close icon
+    gsap
+      .timeline()
+      .to([linkOriginalTexts, linkAnimatedTexts], {
+        y: '-200%',
+        stagger: 0.1,
+        duration: 0.5,
+        ease: 'power4.out',
+      })
+      .add(() => {
+        gsap.set(iconContainer, { display: 'none' });
+        gsap.set(menuContainer, { display: 'block' });
+        linkContainers.forEach((container) => {
+          if (container !== menuContainer) {
+            gsap.set(container, { display: 'none' });
+          }
+        });
+      }, '-=0.5')
+      .to(
+        navbarContainer,
+        {
+          width: '180px',
+          duration: 1,
+          ease: 'power4.inOut',
+        },
+        0
+      )
+      .to(
+        expandedFillPath,
+        {
+          morphSVG: defaultFillPath,
+          duration: 1,
+          ease: 'power4.inOut',
+        },
+        0
+      )
+      .to(
+        fillSvgElement,
+        {
+          attr: { viewBox: '0 0 180 64' },
+          duration: 1,
+          ease: 'power4.inOut',
+        },
+        0
+      )
+      .to(
+        fillSvgElement,
+        {
+          width: 180,
+          duration: 1,
+          ease: 'power4.inOut',
+        },
+        0
+      )
+      .to(
+        fillGElement,
+        {
+          attr: { filter: 'url(#defaultBackgroundFilter)' },
+          duration: 1,
+          ease: 'power4.inOut',
+        },
+        0
+      )
+      .to(
+        expandedStrokePath,
+        {
+          morphSVG: defaultStrokePath,
+          duration: 1,
+          ease: 'power4.inOut',
+        },
+        0
+      )
+      .to(
+        strokeSvgElement,
+        {
+          attr: { viewBox: '0 0 180 64' },
+          duration: 1,
+          ease: 'power4.inOut',
+        },
+        0
+      )
+      .to(
+        strokeSvgElement,
+        {
+          width: 180,
+          duration: 1,
+          ease: 'power4.inOut',
+        },
+        0
+      )
+      .to(
+        [menuContainer],
+        {
+          opacity: 1,
+          duration: 0.5,
+          ease: 'power4.inOut',
+        },
+        0
+      )
+      .add(() => {
+        gsap.set(menuOriginalSplit.chars, { y: '100%' });
+        gsap.to(menuOriginalSplit.chars, {
+          y: '0',
+          stagger: 0.1,
+          duration: 0.5,
+          ease: 'power4.out',
+        });
+      });
   });
 });
