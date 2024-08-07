96c4d04d0c54345fc2d91b13c28f83f66d1e091f
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-08-08 03:19:00 +0800

    Create fullHeightMin.js
    Minified version of fullHeight

---
+++ fullHeightMin.js
@@ -0,0 +1,1 @@
+function setFullHeight(){const e=.01*window.innerHeight;document.documentElement.style.setProperty("--vh",`${e}px`),document.querySelectorAll(".full-height").forEach((t=>{t.style.height=100*e+"px"}))}setFullHeight(),window.addEventListener("resize",(()=>{setFullHeight()}));
