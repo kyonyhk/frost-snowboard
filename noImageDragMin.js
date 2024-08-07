2e2a214d1ce65aac51af4a204a4aa1e4a9a8b0ae
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-08-08 03:21:15 +0800

    Create noImageDragMin.js
    Minified version of noImageDrag

---
+++ noImageDragMin.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",(function(){for(var e=document.getElementsByTagName("img"),t=0;t<e.length;t++)e[t].addEventListener("dragstart",(function(e){e.preventDefault()}))}));
