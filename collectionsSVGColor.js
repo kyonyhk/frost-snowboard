48f225a24f424b2c28fdc4a277816079b20ea6ec
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-08-11 05:34:30 +0800

    Update collectionsSVGColor.js
    Removing console.logs

--- collectionsSVGColor.js
+++ collectionsSVGColor.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   var svgElements = document.querySelectorAll('.cp-main_svg'); // Select all SVG elements
-  console.log("SVG Elements:", svgElements); // Check if the SVG elements are being selected
 
   var currentUrl = window.location.href;
-  console.log("Current URL:", currentUrl); // Log the current URL
 
   svgElements.forEach(function(svg) {
     var paths = svg.querySelectorAll('path'); // Select all path elements within each SVG
