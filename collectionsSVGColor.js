2e3237dde1f97193d689de205cdade7a77d066b8
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-08-11 04:44:00 +0800

    Update collectionsSVGColor.js
    Removing console.logs

--- collectionsSVGColor.js
+++ collectionsSVGColor.js
@@ -9,13 +9,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var paths = svg.querySelectorAll('path'); // Select all path elements within each SVG
     if (currentUrl.includes('ember-collection')) {
       paths.forEach(path => path.style.fill = '#D97848');
-      console.log("Color set to #D97848 for all paths");
     } else if (currentUrl.includes('nebula-collection')) {
       paths.forEach(path => path.style.fill = '#580DEB');
-      console.log("Color set to #580DEB for all paths");
     } else if (currentUrl.includes('apex-collection')) {
       paths.forEach(path => path.style.fill = '#6BE688');
-      console.log("Color set to #6BE688 for all paths");
     }
   });
 });
